28bf04982d72eb8907b94c3d20942b6954991ab8
Jeff Knox <jeffreyaknox7@gmail.com>
2023-03-31 11:28:46 -0700

    Pseudocode for Rock Paper Scissors

---
+++ lesson_2/rock_paper_scissors/rps.rb
@@ -0,0 +1,17 @@
+=begin
+initialize choices as a hash, key is the choice as string, value is the option as string that choice wins against
+prompt user for a choice, rock, paper, or scissors.
+Have a random choice decided by the computer
+Compare user choice to computer choice
+(Rock beats scissors)
+(Scissors beats paper)
+(Paper beats rock)
+Assign winner or loser to user based on this comparison
+Display results
+Prompt user if they want to go again or not.
+
+Self-bonus:
+Track results,
+display winrate to user 
+=end
+
