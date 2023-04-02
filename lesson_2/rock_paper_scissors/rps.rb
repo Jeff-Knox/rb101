eccaf0aa722735b491196246bf9624e59b212979
Jeff Knox <jeffreyaknox7@gmail.com>
2023-04-02 14:47:27 -0700

    Crazy RPS!
    I went a bit crazy with the rock paper scissors, but I wanted to challenge myself a bit so my game can take as many options for "weapons" as you can think of, as long as it is an odd number of options.
    All you have to do is edit the choice_doc.yml, add in pairs of "weapons", as many as you want, and the game will run smoothly. Currently has 27 weapons and works, tested around 50 times.
    The table code is not my own, but that wasn't my focus for this task.
    Code is a bit all over the place, still not great at creating truly abstract methods, and organizing methods by what they do, but I wanted to move on as I feel I've gone a bit out of scope of these lessons anyways.

--- lesson_2/rock_paper_scissors/rps.rb
+++ lesson_2/rock_paper_scissors/rps.rb
@@ -2,12 +2,18 @@ require 'yaml'
 
 PROMPT = YAML.load_file('prompts.yml')
 ERROR = YAML.load_file('errors.yml')
-CHOICES = ['Rock', 'Paper', "Scissors"]
+CHOICE_DOC = YAML.load_file('choice_doc.yml')
+CHOICES = CHOICE_DOC['choices']
+CHOICE_S = "[" << CHOICES.join("][") << "]"
 
 def assign_weapon_winners(choices)
   weapon_winners_chart = {}
   choices.each_with_index do |weapon, i|
-    weapon_winners_chart[weapon] = choices[i - 1]
+    loser_arr = []
+    (choices.length / 2).times do |j|
+      loser_arr << choices[i - ((2 * j) + 1)]
+    end
+    weapon_winners_chart[weapon] = loser_arr
   end
   weapon_winners_chart
 end
@@ -23,39 +29,64 @@ def display(message_type, message, interps = {})
     print "=> " + message + "\n"
   when :e
     print "ERROR: " + message + "\n"
+  when :t
+    print_table(message)
   end
 end
 
-def ask_input(prompt_message, error_message, input_valid)
+def print_table(hash)
+  # Determine the maximum length of the key and values in the hash
+  key_length = hash.keys.map(&:length).max
+  value_length = hash.values.flatten.map(&:length).max
+
+  # Calculate the border length based on the maximum key and value length
+  border_length = key_length + value_length + 7
+
+  # Print the header row
+  printf("| %#{key_length}s | %#{value_length}s |\n", "Option", "Beats")
+  printf("|%s|\n", "-" * border_length)
+
+  # Print each row of the table
+  hash.each do |key, value|
+    words = value.join(', ')
+    printf("| %#{key_length}s | %-#{value_length}s |\n", key, words)
+  end
+
+  # Print the bottom border
+  printf("|%s|\n", "-" * border_length)
+end
+
+def ask(prompt_message, error_message, input_valid, options = {})
   user_input = ''
   loop do
-    display(:p, prompt_message)
-    user_input = gets.chomp
+    display(:p, prompt_message, options)
+    user_input = gets.chomp.downcase
     return user_input if input_valid.call(user_input)
     display(:e, error_message, { invalid_input: user_input })
   end
 end
 
-def normalize_user_choice(choice)
-  WEAPON_WINNERS.each do |option, _|
-    return option if choice.downcase.start_with?(option[0].downcase)
-  end
-end
-
 valid_wep = lambda do |input = ''|
-  return true if CHOICES.include?(input.capitalize)
+  true if CHOICES.include?(input)
 end
 
 valid_yn = lambda do |input = ''|
-  input = input.downcase
   return true if input.start_with?('y') || input.start_with?('n')
   false
 end
 
+def welcome_message
+  display(:m, PROMPT['welcome'])
+  display(:m, PROMPT['display_rules1'], { option_count: CHOICES.length.to_s })
+  display(:m, PROMPT['display_rules2'], { options: CHOICE_S })
+  display(:m, PROMPT['display_rules3'])
+  display(:t, WEAPON_WINNERS)
+end
+
 def assign_choices(valid_wep)
-  choice = ask_input(PROMPT['choose_wep'], ERROR['invalid_wep'], valid_wep)
+  c = ask(PROMPT['which_wep'], ERROR['wep'], valid_wep, { options: CHOICE_S })
   chosen_weapons = {
-    user: normalize_user_choice(choice),
+    user: c,
     cpu: CHOICES.sample(1)[0]
   }
   chosen_weapons
@@ -63,16 +94,14 @@ end
 
 def calculate_winner(choices)
   return 'tie' if choices[:user] == choices[:cpu]
-  WEAPON_WINNERS.each do |winner, loser|
-    return 'user_wins' if choices[:user] == winner && choices[:cpu] == loser
-    return 'user_loses' if choices[:cpu] == winner && choices[:user] == loser
+  WEAPON_WINNERS.each do |win, lose|
+    return 'user_won' if choices[:user] == win && lose.include?(choices[:cpu])
+    return 'user_lost' if choices[:cpu] == win && lose.include?(choices[:user])
   end
 end
 
 def play_again?(valid_yn)
-  play_again = ask_input(PROMPT['play_again'], ERROR['invalid_y_n'], valid_yn)
-  puts play_again
-  play_again.downcase!
+  play_again = ask(PROMPT['play_again'], ERROR['y_n'], valid_yn)
   user_choice = true if play_again.start_with?('y')
   user_choice = false if play_again.start_with?('n')
   user_choice
@@ -80,8 +109,8 @@ end
 
 def calculate_user_stats(winner, w_l_data)
   case winner
-  when 'user_wins' then w_l_data[:win] += 1.0
-  when 'user_loses' then w_l_data[:loss] += 1
+  when 'user_won' then w_l_data[:win] += 1.0
+  when 'user_lost' then w_l_data[:loss] += 1
   else w_l_data[:tie] += 1
   end
   w_l_data[:total] = w_l_data[:win] + w_l_data[:loss] + w_l_data[:tie]
@@ -98,7 +127,7 @@ user_stats = {
   wr: 0
 }
 
-display(:m, PROMPT['welcome'])
+welcome_message
 loop do
   weapon_choices = assign_choices(valid_wep)
   display(:m, PROMPT['choices'], weapon_choices)
